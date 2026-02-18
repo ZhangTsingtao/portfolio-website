554ce9a0bb0c2eb358f49004b76866b9ac22ab18
Tsingtao Zhang <rickzhangqt@gmail.com>
2026-02-18 15:57:56 -0500

    new stuff

---
+++ assets/js/footer.js
@@ -0,0 +1,19 @@
+document.getElementById('site-footer').outerHTML = `
+	<section id="footer">
+		<div class = "container">
+			<div class="row gtr-150">
+				<div class="col-6 col-12-medium">
+					<p style = "text-align:left; margin: 0 0 0 0; font-size: 1.25em;" >
+						<b>Tsingtao Zhang</b>
+					</p>
+					<p style = "text-align:left;  margin: 0 0 0 0;" >
+						Technical Artist / Gameplay Programmer
+					</p>
+				</div>
+				<div class="col-6 col-12-medium">
+					Email: <a href="mailto: tsingzhang1@gmail.com"> tsingzhang1@gmail.com </a>
+					<br>Phone: 585-615-0628
+				</div>
+			</div>
+		</div>
+	</section>`;
---
+++ assets/js/header.js
@@ -0,0 +1,15 @@
+document.getElementById('site-header').outerHTML = `
+		<!-- Header -->
+		<section id="one" class="main style3" style="background-color: #E0DAB8;">
+			<div class="container">
+				<div class="row gtr-150">
+					<div class="col-8 col-12-medium imp-medium">
+						<a href="index.html" style="text-align: right; margin-right: 5%; font-size:30px" target="_self"><font color="#475527">About Me</font></a>
+						<a href="PCG.html" style="text-align: right; margin-right: 5%; font-size:30px" target="_self"><font color="#475527">Realtime Procedural Generation</font></a>
+						<a href="Duolatera.html" style="text-align: right; margin-right: 5%; font-size:30px" target="_self"><font color="#475527">Duolatera</font></a>
+						<a href="Artwork.html" style="text-align: right; margin-right: 5%; font-size:30px" target="_self"><font color="#475527">Artwork</font></a>
+						<a href="VRExercise.html" style="text-align: right; margin-right: 5%; font-size:30px" target="_self"><font color="#475527">VR Exercise Research</font></a>
+					</div>
+				</div>
+			</div>
+		</section>`;
